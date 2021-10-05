182ab7425c29c746b5a47105935e58c2cc75b8ce
Davio Lopes <davioplp@gmail.com>
2021-10-05 00:04:49 -0300

    projetos

---
+++ Projetos/Zoo/src/getEmployeeByName.js
@@ -0,0 +1,11 @@
+const { employees } = require('../data/zoo_data');
+
+function getEmployeeByName(employeeName) {
+  if (employeeName !== undefined) {
+    return employees.find((iten) => employeeName === iten.firstName
+     || employeeName === iten.lastName);
+  }
+  return {};
+}
+console.log(getEmployeeByName('Emery'));
+module.exports = getEmployeeByName;
